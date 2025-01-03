c7398604ff8a49e4850575f7ed061f2f52c0740a
Subham Sharma <141348353+subham-sharma21@users.noreply.github.com>
2025-01-04 02:07:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        decimal1 = int(a, 2)
+        decimal2 = int(b, 2)
+        add = decimal1+decimal2
+        x = str(bin(add))
+        return x
+
+        
