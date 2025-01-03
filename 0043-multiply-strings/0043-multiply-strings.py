b04d45cfe0d036c1304a1c57f147440a938d2ead
Subham Sharma <141348353+subham-sharma21@users.noreply.github.com>
2025-01-04 01:30:40 +0530

    Time: 0 ms (100%), Space: 17.5 MB (30.49%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # num1.replace('"','')
+        # num2.replace('"','')
+        mul = int(num1)*int(num2)
+
+        return str(mul)
+
+        
